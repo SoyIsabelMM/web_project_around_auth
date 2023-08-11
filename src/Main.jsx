4f7f39a70b14527c240600a23ddfed64fd130293
Isa <isa@MacBook-Air-de-Daniel.local>
2023-08-11 09:47:12 -0400

    feat: Crear el componente Popup y agregarlo en el main

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Profile from "./Profile";
 import Elements from "./Elements";
+import Popup from "./Popup";
 
 function Main() {
   return (
     <main className="content">
       <Profile />
       <Elements />
+      <Popup />
     </main>
   );
 }
