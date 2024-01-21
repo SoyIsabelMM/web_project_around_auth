4ca93e85cbc3fcc06e7e68ef20b0731bfc3b6f97
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2024-01-21 17:28:42 -0300

    feat: Crear logica de lógin para abrir y cerrar menu hamburguesa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/header-logo.png";
 import { useMatch, Link } from "react-router-dom";
+import menu from "../images/nav-bar.png";
+import closeBtn from "../images/close-icon.png";
 
-function Header() {
+function Header({ email, signOut }) {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    setOpen(!open);
+  };
   return (
     <>
-      <header className="header">
+      <header className={`header ${open ? "header_menu-open" : ""}`}>
         <img
           className="header__logo-header"
           src={logo}
@@ -22,6 +29,32 @@ function Header() {
           <Link to="/signin" className="link link__auth">
             Iniciar Sesión
           </Link>
+        )}
+
+        {useMatch("/") && (
+          <>
+            <div className={`header__nav-bar ${open && ""}`}>
+              <span className="header__email">isabelmartinez</span>
+              <button className="header__sign-out">Cerrar Sesión</button>
+              <div className="header__divider-menu"></div>
+            </div>
+
+            {open ? (
+              <img
+                src={closeBtn}
+                alt="Botón cerrar menú"
+                onClick={handleMenu}
+                className="header__close-btn"
+              />
+            ) : (
+              <img
+                src={menu}
+                alt="menú desplegable forma hamburguesa"
+                onClick={handleMenu}
+                className="header__menu"
+              />
+            )}
+          </>
         )}
       </header>
       <div className="divider-section"></div>
